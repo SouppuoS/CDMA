93dcf6923f78754b2d04c09cdf2d89da1ef1e6a3
SSSS <zhou_huy@163.com>
2021-10-26 10:21:44 +0800

    add min-norm filter

--- README.md
+++ README.md
@@ -2,16 +2,24 @@
 Generate weights for Differential Microphone Arrays (CDMA)[1] in python. Allow to design cardioid, supercardioid or hypercardioid beamformer.  
 ![cardioid2nd](beampattern.png)
 
-## 2-STEP TO USE
+## HOW TO USE
 ```
 cma     = circular_microphone_arrays(M=4)
 cdma    = CDMA(cma, sa=90, null_list=[180, 270], symmetric=np.array([[-1,0,1, 0]]))
 Beampattern(cma, cdma.get_weight(), freq=1000)
 ```
-First, define a `circular_microphone_arrays` base on geometry of your microphone array, eg: `r` for radius (cm), `M` for the number of microphones. For now, only uniform circular array (UCA) is supported.  
+- **2-STEP TO USE:** First, define a `circular_microphone_arrays` base on geometry of your microphone array, eg: `r` for radius (cm), `M` for the number of microphones. For now, only uniform circular array (UCA) is supported.  
 Second, design your CDMA base on your microphone array, including `sa` for steer angle, `null_list` for null point list and `symmetric` for H property.  
 The order of CDMA is depend on `null_list` and `symmetric`.  
 For `symmetric` design, you can see *Section 2.1* and *Section 2.2* in [1] for help.  
+  
+- To build a **Minimum-norm Filter**, which is robust against white noise amplification, use the code below. The formulation of minimum-norm can be found in *Section 7.1* from [1].
+```
+cma     = circular_microphone_arrays(M=4)
+cdma    = CDMA(cma, sa=90, null_list=[225], 
+               symmetric=np.array([[-1, 0, 1, 0]]),
+               sym_b=np.array([[0]]), min_norm=True)
+```
 
 ## TO-DO
 - mask for microphones  
--- cdma.py
+++ cdma.py
@@ -49,8 +49,10 @@ class CDMA():
         cma         : circular_microphone_arrays,
         sa          : float,                                # steer angle (distortless directgion)
         symmetric   : np.ndarray,                           # symmetric constrain for the weight
+        sym_b       : np.ndarray                 = None,    # b part of symmetric constrain
         null_list   : list                       = [],      # list of null point (degree)
         mic_mask    : list                       = [],      # list of microphone mask
+        min_norm    : bool                       = False,   # use minimum-norm filter
         ) -> None:
         self.cma        = cma
         self.null_list  = null_list
@@ -62,19 +64,28 @@ class CDMA():
         for d in [sa,] + self.null_list:
             _eq.append(self.cma.get_steer(d))
         _eq         = np.array(_eq)
-        _eq         = np.concatenate((_eq, _sy), axis=0)        # [M x M x f_bin]
-        _eq[...,0]  = np.eye(self.cma.M)
-        _b          = np.zeros((self.cma.M, 1, _eq.shape[-1]))  # [M x 1 x f_bin]
+        _eq         = np.concatenate((_eq, _sy), axis=0)            # [N x M x f_bin], N : number of constrains
+        _eq[...,0]  = np.eye(self.cma.M)[:_eq.shape[0]]
+        _b          = np.zeros((_eq.shape[0], 1, _eq.shape[-1]))    # [N x 1 x f_bin]
         _b[0]       = 1.
         _b[...,0]   = 1. / self.cma.M
+
+        if sym_b is not None:
+            n_sym_b = sym_b.shape[-1]
+            for i in range(n_sym_b):
+                _b[- n_sym_b + i] = sym_b[i]
         
         if len(mic_mask) > 0:
             _eq = _eq[mic_mask]
             _eq = _eq[:, mic_mask]
             _b  = _b[mic_mask]
 
-        # [f_bin x M x 1]
-        self.weight = np.linalg.solve(_eq.conjugate().transpose(2, 0, 1), _b.transpose(2, 0, 1))
+        if min_norm:
+            _mAA        = np.einsum('ijk,ljk->kil', _eq, _eq.conjugate())  # [f_bin x N x N]
+            self.weight = np.einsum('ijk,kil,lnk->kjn', _eq.conjugate(), np.linalg.inv(_mAA), _b)
+        else:
+            # [f_bin x M x 1]
+            self.weight = np.linalg.solve(_eq.conjugate().transpose(2, 0, 1), _b.transpose(2, 0, 1))
     
     def get_weight(self):
         return self.weight
@@ -88,13 +99,15 @@ def Beampattern(
     # [sa_bin x M] x [M x 1]
     a_map     = np.einsum('ij,jk->ik', cma.steer[..., tgt_f_bin], w[tgt_f_bin].conjugate())
     plt.subplot(1,1,1, polar=True)
-    plt.plot(np.linspace(0, 2 * np.pi, 360), 10 * np.log10(np.abs(a_map).reshape(-1) + 1e-4))
+    plt.plot(np.linspace(0, 2 * np.pi, 360), 10 * np.log10(np.abs(a_map).reshape(-1) + 1e-2))
     plt.title('4-MIC CDMA 2nd-cardioid\nsteer angle = 90 deg null point at 180 deg and 270 deg')
     plt.tight_layout()
     plt.savefig('beampattern')
 
 if __name__ == "__main__":
     cma     = circular_microphone_arrays(M=4)
-    cdma    = CDMA(cma, sa=90, null_list=[180, 270], symmetric=np.array([[-1,0,1, 0]]))
+    cdma    = CDMA(cma, sa=90, null_list=[225], 
+                   symmetric=np.array([[-1, 0, 1, 0]]),
+                   sym_b=np.array([[0]]), min_norm=True)
     Beampattern(cma, cdma.get_weight(), freq=1000)
     pass
